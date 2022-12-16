ec6d01374a3cea834741e9dbcda7d723b4baf12f
AnnaAFA <anna.fedoruk111@gmail.com>
2022-12-16 20:58:36 +0200

    1 and 2 tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,47 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+console.log(galleryRef);
+
+const markup = galleryItems.reduce(
+  (acc, { preview, original, description }) =>
+    acc +
+    `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+    </a>
+    </div>`,
+  ''
+);
+// console.log(markup);
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+const onGalleryClick = e => {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src='${e.target.dataset.source}' width='800', height ='600'>`,
+    {
+      onShow: instance => {
+        galleryRef.addEventListener('keydown', onEscapeClick);
+      },
+      onClose: instance => {
+        galleryRef.removeEventListener('keydown', onEscapeClick);
+      },
+    }
+  );
+  instance.show();
+
+  function onEscapeClick(e) {
+    if (e.code === 'Escape') {
+      instance.close();
+    }
+  }
+};
+
+galleryRef.addEventListener('click', onGalleryClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+console.log(galleryRef);
+
+const markup = galleryItems.reduce(
+  (acc, { preview, original, description }) =>
+    acc +
+    `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`,
+  ''
+);
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+function onStopRestart(e) {
+  e.preventDefault();
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
